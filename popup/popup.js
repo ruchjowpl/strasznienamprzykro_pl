d25c743431b345668aacf7c7ef325835c56cd952
mmilian <mateusz.milian@gmail.com>
2015-08-31 23:49:27 +0200

    GA events added

--- popup/popup.js
+++ popup/popup.js
@@ -60,7 +60,8 @@ $('.button_animacja').click(function(e) {
 });
 
 function bindAction(e,i) {
-    showPopup(e,i);  
+     showPopup(e,i);  
+     _trackEvent("popup", "clicked", undefined, i);
      return false;
 };
 
@@ -91,6 +92,9 @@ function showPopup(e,i) {
       function onPlayerReady(event) { }
 
       function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING)  {
+            _trackEvent("popup", "play", undefined, i);
+        }
         if (event.data == YT.PlayerState.ENDED) {
                 // insert appropriate modal (or whatever) below
            /* $(".#container2").append('<div class="content"><div id="player" class="player"> </div> <div style="margin-left:90%;background-color:red"><ul><li><a class="btnz share facebook" href="http://www.facebook.com/sharer.php?u=http://strasznienamprzykro.pl/"><i class="fa fa-facebook fa-3x"></i></a></li><li><a class="btnz share twitter" href="http://twitter.com/share?text=%23TAKdlaJOW&url=http://strasznienamprzykro.pl"><i class="fa fa-twitter fa-3x"></i></a></li></ul></div><br><div class="support_popup"><span class="content_font_title"> PODOBA CI SIĘ NASZ PRZEKAZ?</span> <br/><span class="content_font">Wesprzyj produkcję i promocję kolejnych spotów. <br/>Każda złotówka pozwala nam dotrzeć do 10 nowych osób!</span>  <br/><a class="payment_button first" href="#"><i class="fa fa-arrow-right"></i> wpłać <strong>10 zł</strong></a><a class="payment_button" href="#"><i class="fa fa-arrow-right"></i> wpłać <strong>50 zł </strong></a><a class="payment_button" href="#"><i class="fa fa-arrow-right"></i> dowolna</a></div><div class="arrow"><a href="#" style="font-size:18px" >Następny <br> spot<i class="fa fa-forward"></i></a></div></div>');
